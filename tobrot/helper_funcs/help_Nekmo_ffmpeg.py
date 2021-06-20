6420b1bb766288522ed0fa19292627e32cb49bb5
SayanthD <13906889+SayanthD@users.noreply.github.com>
2021-06-20 13:13:25 +0530

    screenshot: Remove redundant mimetype check

--- tobrot/helper_funcs/help_Nekmo_ffmpeg.py
+++ tobrot/helper_funcs/help_Nekmo_ffmpeg.py
@@ -2,31 +2,27 @@
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
 
-import os
 import time
+from pathlib import Path, PurePath
 
 from tobrot.helper_funcs.run_shell_command import run_command
 
 
 async def take_screen_shot(video_file, output_directory, ttl):
     # https://stackoverflow.com/a/13891070/4723940
-    out_put_file_name = os.path.join(output_directory, str(time.time()) + ".jpg")
-    if video_file.upper().endswith(("MKV", "MP4", "WEBM")):
-        file_generator_command = [
-            "ffmpeg",
-            "-ss",
-            str(ttl),
-            "-i",
-            video_file,
-            "-vframes",
-            "1",
-            out_put_file_name,
-        ]
-        # width = "90"
-        _, __ = await run_command(file_generator_command)
-        # Wait for the subprocess to finish
+    output_file = PurePath(output_directory).joinpath(str(time.time()) + ".jpg")
+    cmd_generate_thumbnail = [
+        "ffmpeg",
+        "-ss",
+        str(ttl),
+        "-i",
+        video_file,
+        "-vframes",
+        "1",
+        output_file,
+    ]
+    # width = "90"
+    await run_command(cmd_generate_thumbnail)
+    # Wait for the subprocess to finish
     #
-    if os.path.lexists(out_put_file_name):
-        return out_put_file_name
-    else:
-        return None
+    return str(output_file) if Path(output_file).exists() else None
