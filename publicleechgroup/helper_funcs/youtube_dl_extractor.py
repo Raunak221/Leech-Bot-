12f95145a07e463e769b986906f433def2f2fbc9
SayanthD <13906889+SayanthD@users.noreply.github.com>
2021-07-22 14:19:01 +0530

    youtube_dl_extractor.py: Minor reformat and fixes
    * Remove a 'if' condition and untab
    
    * Add 'noplaylist' to opts

--- publicleechgroup/helper_funcs/youtube_dl_extractor.py
+++ publicleechgroup/helper_funcs/youtube_dl_extractor.py
@@ -19,9 +19,10 @@ import asyncio
 import functools
 from concurrent.futures import ThreadPoolExecutor
 
-import yt_dlp
 from pykeyboard import InlineKeyboard
 from pyrogram.types import InlineKeyboardButton
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import DownloadError, ExtractorError
 
 from publicleechgroup import Config
 from publicleechgroup.helper_funcs.display_progress import humanbytes
@@ -40,7 +41,7 @@ def run_in_executor(_func):
 
 @run_in_executor
 def yt_extract_info(video_url, download, ytdl_opts, ie_key):
-    with yt_dlp.YoutubeDL(ytdl_opts) as ytdl:
+    with YoutubeDL(ytdl_opts) as ytdl:
         info = ytdl.extract_info(video_url, download=download, ie_key=ie_key)
     return info
 
@@ -48,7 +49,7 @@ def yt_extract_info(video_url, download, ytdl_opts, ie_key):
 async def extract_youtube_dl_formats(
     url, yt_dl_user_name, yt_dl_pass_word, user_working_dir
 ):
-    info_dict = {}
+    info_dict = {"noplaylist": True}
     if yt_dl_user_name and yt_dl_pass_word:
         info_dict.update(
             {
@@ -59,6 +60,7 @@ async def extract_youtube_dl_formats(
     if "hotstar" in url:
         info_dict.update(
             {
+                "geo_bypass": True,
                 "geo_bypass_country": "IN",
             }
         )
@@ -69,99 +71,92 @@ async def extract_youtube_dl_formats(
             ytdl_opts=info_dict,
             ie_key=None,
         )
-    except yt_dlp.utils.DownloadError as ytdl_error:
+    except (DownloadError, ExtractorError) as ytdl_error:
         return None, str(ytdl_error), None
 
-    if info:
-        ikeyboard = InlineKeyboard()
-        #
-        thumb_image = info.get("thumbnail", None)
-        # LOGGER.info(thumb_image)
-        # YouTube acts weirdly,
-        # and not in the same way as Telegram
-        thumbnail = thumb_image or Config.DEFAULT_THUMBNAIL
+    ikeyboard = InlineKeyboard()
+    #
+    thumbnail = info.get("thumbnail", Config.DEFAULT_THUMBNAIL)
+    # LOGGER.info(thumb_image)
+    # YouTube acts weirdly,
+    # and not in the same way as Telegram
 
-        extractor_key = info.get("extractor_key", "Generic")
-        duration = info.get("duration", None)
-        if info.get("formats"):
-            for formats in info.get("formats"):
-                format_id = formats.get("format_id")
-                format_string = formats.get("format_note")
-                if format_string is None:
-                    format_string = formats.get("format")
-                # don't display formats, without audio
-                # https://t.me/c/1434259219/269937
-                if "DASH" in format_string.upper():
-                    continue
-                format_ext = formats.get("ext")
-                approx_file_size = (
-                    humanbytes(formats.get("filesize"))
-                    if formats.get("filesize")
-                    else ""
+    extractor_key = info.get("extractor_key", "Generic")
+    duration = info.get("duration", None)
+    if info.get("formats"):
+        for formats in info.get("formats"):
+            format_id = formats.get("format_id")
+            format_string = formats.get("format_note", formats.get("format"))
+            # don't display formats, without audio
+            # https://t.me/c/1434259219/269937
+            if "DASH" in format_string.upper():
+                continue
+            format_ext = formats.get("ext")
+            approx_file_size = (
+                humanbytes(formats.get("filesize")) if formats.get("filesize") else ""
+            )
+            av_codec = "empty"
+            if formats.get("acodec") == "none" or formats.get("vcodec") == "none":
+                av_codec = "none"
+            dipslay_str_uon = (
+                f"{format_string} [{format_ext.upper()}] {approx_file_size}"
+            )
+            cb_string_video = f"video|{extractor_key}|{format_id}|{av_codec}"
+            # GDrive gets special pass, acodec is not listed here, ie acodec=None
+            if (
+                extractor_key == "GoogleDrive"
+                and format_id == "source"
+                or extractor_key != "GoogleDrive"
+                and format_string
+                and "audio only" not in format_string
+            ):
+                ikeyboard.row(
+                    InlineKeyboardButton(dipslay_str_uon, callback_data=cb_string_video)
                 )
-                av_codec = "empty"
-                if formats.get("acodec") == "none" or formats.get("vcodec") == "none":
-                    av_codec = "none"
-                dipslay_str_uon = (
-                    f"{format_string} [{format_ext.upper()}] {approx_file_size}"
-                )
-                cb_string_video = f"video|{extractor_key}|{format_id}|{av_codec}"
-                # GDrive gets special pass, acodec is not listed here, ie acodec=None
-                if (
-                    extractor_key == "GoogleDrive"
-                    and format_id == "source"
-                    or extractor_key != "GoogleDrive"
-                    and format_string
-                    and "audio only" not in format_string
-                ):
-                    ikeyboard.row(
-                        InlineKeyboardButton(
-                            dipslay_str_uon, callback_data=cb_string_video
-                        )
-                    )
-                elif extractor_key != "GoogleDrive":
-                    # special weird case :\
-                    ikeyboard.row(
-                        InlineKeyboardButton(
-                            f"SVideo ({approx_file_size})",
-                            callback_data=cb_string_video,
-                        )
-                    )
-            if duration:
+            elif extractor_key != "GoogleDrive":
+                # special weird case :\
                 ikeyboard.row(
                     InlineKeyboardButton(
-                        "MP3 (64 kbps)", callback_data=f"audio|{extractor_key}|64|mp3"
-                    ),
-                    InlineKeyboardButton(
-                        "MP3 (128 kbps)", callback_data=f"audio|{extractor_key}|128|mp3"
-                    ),
-                )
-                ikeyboard.row(
-                    InlineKeyboardButton(
-                        "MP3 (320 kbps)", callback_data=f"audio|{extractor_key}|320|mp3"
+                        f"SVideo ({approx_file_size})",
+                        callback_data=cb_string_video,
                     )
                 )
-        elif "entries" in info:
-            for entries in info.get("entries"):
-                for formats in entries.get("formats"):
-                    format_id = formats.get("format_id")
-                    format_ext = formats.get("ext")
-                    cb_string_file = f"generic|{extractor_key}|{format_id}|{format_ext}"
-                    ikeyboard.row(
-                        InlineKeyboardButton(
-                            f"YTDL Generic File [{format_ext.upper()}]",
-                            callback_data=cb_string_file,
-                        ),
-                    )
-        else:
-            format_id = info.get("format_id", None)
-            # format_ext = info.get("ext", None)
+        if duration:
             ikeyboard.row(
                 InlineKeyboardButton(
-                    "SVideo", callback_data=f"video|{extractor_key}|{format_id}|DL"
+                    "MP3 (64 kbps)", callback_data=f"audio|{extractor_key}|64|mp3"
+                ),
+                InlineKeyboardButton(
+                    "MP3 (128 kbps)", callback_data=f"audio|{extractor_key}|128|mp3"
+                ),
+            )
+            ikeyboard.row(
+                InlineKeyboardButton(
+                    "MP3 (320 kbps)", callback_data=f"audio|{extractor_key}|320|mp3"
                 )
             )
-            # LOGGER.info(ikeyboard)
+    elif "entries" in info:
+        for entries in info.get("entries"):
+            for formats in entries.get("formats"):
+                format_id = formats.get("format_id")
+                format_ext = formats.get("ext")
+                cb_string_file = f"generic|{extractor_key}|{format_id}|{format_ext}"
+                ikeyboard.row(
+                    InlineKeyboardButton(
+                        f"YTDL Generic File [{format_ext.upper()}]",
+                        callback_data=cb_string_file,
+                    ),
+                )
+    else:
+        format_id = info.get("format_id", None)
+        # format_ext = info.get("ext", None)
+        ikeyboard.row(
+            InlineKeyboardButton(
+                "YTDL Generic File",
+                callback_data=f"video|{extractor_key}|{format_id}|DL",
+            )
+        )
+        # LOGGER.info(ikeyboard)
     succss_mesg = (
         "Select the desired format: 👇\n"
         "<u>mentioned</u> <i>file size might be approximate</i>"
